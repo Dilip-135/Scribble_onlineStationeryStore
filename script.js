9ecd252697eb63163ac9f9eb25395ed6d472974a
Dilip-135 <royvs8402@gmail.com>
2025-02-22 15:39:53 +0530

    Scribble 1.0

---
+++ script.js
@@ -0,0 +1,28 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+document.addEventListener("DOMContentLoaded", () => {
+    const fadeElements = document.querySelectorAll(".fade-in");
+    
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    }, { threshold: 0.5 });
+    
+    fadeElements.forEach(element => observer.observe(element));
+});
